274b98047e63fded7e6949dea91023d90a7eb208
MoesioMarcelino <moesiomarcelino1@gmail.com>
2019-03-06 21:09:39 -0300

    challenge-17

--- challenge-17/challenge-17.js
+++ challenge-17/challenge-17.js
@@ -42,7 +42,7 @@
     minúsculo por "0" (número zero). Mostre o resultado no console:
     */
     console.log( '\nTrocando de "D" a "h" por "0":' );
-    console.log(text.replace(/(D-Z)(a-h)/g, 0));
+    console.log(text.replace(/[D-h]/g, 0));
 
     /*
     Substitua todos os "A" (maiúsculos ou minúsculos) por "4".
